377c1e8afeb6d80a43be92af3471366426df2e54
Peter Erdodi <65724132+erdodip@users.noreply.github.com>
2021-05-30 20:03:30 +0200

    Upload round-robin load balancer with Docker

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # SysAdmin
 1. Provisioning with Vagrant and Nginx
 2. Simple shell widgets
-3. Docker tomcat and postgresql
+3. Docker Tomcat and Postgresql
 4. Docker and WordPress
+5. Docker and Load Balacing
---
+++ load_balancing_nginx_docker/web2/tunnel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Write your SSH remote port forward tunnelling command here, e.g.
+# ssh -TNi ../id_rsa -R <REMOTE_PORT>:localhost:<LOCAL_PORT> codecool@<REMOTE_IP>
+
+autossh -vTNi ../id_rsa -R 13082:localhost:8082 codecool@18.184.218.192 || (sudo apt-get update && sudo apt-get install -y autossh && autossh -vTNi ../id_rsa -R 13082:localhost:8082 codecool@18.184.218.192)
